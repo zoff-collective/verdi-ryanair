3503f0d791aaede2bd8a4281365869712f4842ec
Zoff / Pierre <41169887+Pierre-Zoff@users.noreply.github.com>
2018-09-21 13:31:50 +0200

    Text Edit Start P

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import Hero from '../components/hero';
 import Quote from '../components/quote';
 import Layout from '../components/layout';
 import Text from '../components/text';
+import TextHeadline from '../../components/text-headline';
 
 export default () => (
   <Layout>
@@ -60,41 +61,32 @@ export default () => (
       image={require('../static/images/book-a-date.jpg')}
       title="Want to be part of change at Ryanair?"
       target="https://my-ryanair-cabin-crew-job.typeform.com/to/mkOr7o"
-      label="Get in touch"
+      label="Book a date"
     />
 
     <Quote text="We are a global federation of unions working to improve the lives of aviation and transport sector workers like you." />
 
     <Text title="We are many!">
+            
       <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-        ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
+        We are at every German Ryanair base. We are with Ver.di. With 2 million members Ver.di is the biggest service trade union in the world. The name Ver.di stands for Vereinte Dienstleistungsgewerkschaft – United Services Union. We, ver.di, are one union for all. We consolidate the interest of our members – and by working together empower each of us. We create unity out of diversity. 
       </p>
-
+           
+      
+       <Text title="How do we win!">
+      
+       <TextHeadline>Times are changing</TextHeadline>
+      
       <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-        diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor
-        sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-        diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-        erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-        ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-        ipsum dolor sit amet.
+        For many years, Ryanair didn’t accept unions as negotiation partner and was not willing to negotiate Collective Labour Agreements under national law. But times are changing, and so is Ryanair.
       </p>
+            
     </Text>
 
     <Cta
       image={require('../static/images/become-member.jpg')}
       title="To help win lasting change at Ryanair"
-      target="https://mitgliedwerden.verdi.de/beitritt/verdi-en"
+      target="https://my-ryanair-cabin-crew-job.typeform.com/to/mkOr7o"
       label="Become a ver.di member"
     />
 
