ab0bf63a3e2d29a9c0f94e170ead2c40f791cd14
Zoff / Pierre <41169887+Pierre-Zoff@users.noreply.github.com>
2018-09-21 11:46:01 +0200

    Text Editing

--- pages/guardians/index.jsx
+++ pages/guardians/index.jsx
@@ -68,13 +68,143 @@ export default () => (
             Ich bin Patin für ver.di Tarifkommission bei Ryanair, weil schnell faire Arbeitsbedingungen für Flugbegleiter*innen geschaffen werden müssen..
           `
         },
+        
          {
           name: 'Thorsten Schäfer-Gümbel',
           role: 'Stellvertretender SPD-Bundesvorsitzender',
           quote: `
             Ich unterstütze die Gewerkschaften bei ihren Tarifverhandlungen mit Ryanair, weil Löhne und Arbeitsbedingungen bei uns nicht einseitig festgelegt sondern zwischen den Tarifparteien fair ausgehandelt werden. Die Geltendmachung von Arbeitnehmerinteressen ist geschützt, der Zusammenschluss in Gewerkschaften und Verbänden und der Arbeitskampf haben hier Verfassungsrang. Auch Unternehmen wie Ryanair, die Dumpinglöhne, prekäre Beschäftigung und Druck auf Arbeitnehmerinnen und Arbeitnehmer zum Geschäftsmodell gemacht haben, müssen wissen: Die SPD ist in dieser Frage nicht neutral sondern klar auf der Seite der Beschäftigten.          `
+          `
         },
-      ]}
+
+         {
+          name: 'Bernd Riexinger',
+          role: 'Stellvertretender SPD-Bundesvorsitzender',
+          quote: `
+            Ich unterstütze die Gewerkschaften bei ihren Tarifverhandlungen mit Ryanair, weil Löhne und Arbeitsbedingungen bei uns nicht einseitig festgelegt sondern zwischen den Tarifparteien fair ausgehandelt werden. Die Geltendmachung von Arbeitnehmerinteressen ist geschützt, der Zusammenschluss in Gewerkschaften und Verbänden und der Arbeitskampf haben hier Verfassungsrang. Auch Unternehmen wie Ryanair, die Dumpinglöhne, prekäre Beschäftigung und Druck auf Arbeitnehmerinnen und Arbeitnehmer zum Geschäftsmodell gemacht haben, müssen wissen: Die SPD ist in dieser Frage nicht neutral sondern klar auf der Seite der Beschäftigten.          `
+          `
+        },
+        
+         {
+          name: 'Arno Klare',
+          role: 'Vorsitzender der Partei DIE LINKE',
+          quote: `
+            Die Kabinenbeschäftigten von Ryanair sind mutige Heldinnen und Helden. Sie nehmen den Kampf mit einem schier übermächtigen Konzern auf und streben einen Tarifvertrag an. Ich drücke ihnen die Daumen! Ihr Engagement nutzt allen Beschäftigten in Deutschland. Es kann nicht angehen, dass internationale Konzerne ihre Profite durch Lohndumping auf dem Rücken der Beschäftigten machen. Nur ein Tarifvertrag kann nachhaltige Verbesserungen und gleichwertige Arbeitsbedingungen für alle Flugbegleiterinnen und Flugbegleiter bei Ryanair in Deutschland sicherstellen!        },
+          `
+        },
+        
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+        
+         {
+          name: 'Hakan Bölükmese',
+          role: 'ver.di VL Sprecher Fraport und Mitglied im Aufsichtsrat Fraport',
+          quote: `
+            Ich bin Pate für die ver.di-Tarifkommission bei Ryanair, weil die Kollegen und Kolleginnen schon längst bessere Arbeits- und Tarifbedingungen verdiENT haben.
+              `
+        },
+        
+         {
+          name: 'Behrad Ghofrani',
+          role: 'Betriebsratsvorsitzender WISAG, Flughafen Frankfurt',
+          quote: `
+            Ich bin Pate für die ver.di-Tarifkommission bei Ryanair, weil wir die führende Low Coast Airline abfertigen am Flughafen Frankfurt, daher kenne ich die Folgen für unsere Kollegen aus den Bodenverkehrsdiensten sehr gut.
+              `
+        },
+        
+         {
+          name: 'Christine Behle',
+          role: 'Bundesvorstand der Gewerkschaft ver.di',
+          quote: `
+            Ich bin Patin für die ver.di-Tarifkommission bei Ryanair, weil faire Arbeitsbedingungen und Einkommen bei Ryanair längst überfällig sind. Gemeinsam mit anderen Gewerkschaften in ganz Europa wollen wir gute Tarifverträge durchsetzen              `
+                `
+         },
+        
+         {
+          name: 'Mehdi Nazem Schariflou',
+          role: 'Betriebsratsvorsitzender der Firma Fracares am Flughafen Frankfurt',
+          quote: `
+            Ich bin Pate für die ver.di Tarifkommission bei Ryanair, weil ich an gelebte Solidarität und daran glaube, dass jeder seinen „Gerechten Lohn“ verdient hat!              `
+        },
+        
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+         {
+          name: 'Peter Feldmann',
+          role: 'Oberbürgermeister von Frankfurt',
+          quote: `
+            Tarifverträge, die zwischen Gewerkschaften und Arbeitgeber auf Augenhöhe verhandelt werden, sind Basis für gute Arbeitsbedingungen für jeden Arbeitnehmer und Grundlage für den Erfolg unsers Wirtschaftsmodells. Deshalb freue ich mich, dass Ryanair zu Tarifverhandlungen bereit ist. Hoffentlich gehören prekäre Beschäftigung und Dumpinglöhne endlich der Vergangenheit an.        },
+              `
+        },
+      
+        ]}
     />
 
     <Footer title="Time to change at Ryanair" />
