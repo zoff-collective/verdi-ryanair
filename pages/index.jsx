8ab82d1dad417d5164ef9ee769f0483ddf49476c
Zoff / Pierre <41169887+Pierre-Zoff@users.noreply.github.com>
2018-09-21 17:21:35 +0200

    Link change

--- pages/index.jsx
+++ pages/index.jsx
@@ -201,7 +201,7 @@ export default () => (
     <Cta
       image={require('../static/images/become-member.jpg')}
       title="To help win lasting change at Ryanair"
-      target="https://my-ryanair-cabin-crew-job.typeform.com/to/mkOr7o"
+      target="https://mitgliedwerden.verdi.de/beitritt/verdi"
       label="Become a ver.di member"
     />
 
