f97c0ce01a73116464c648d1f3833ccb66b9218b
Gustav Pursche <pursche@posteo.de>
2018-09-28 15:47:45 +0200

    CTA: add button hover and focus styles

--- components/button/styles.js
+++ components/button/styles.js
@@ -10,12 +10,25 @@ export default css`
     padding: 0.8rem 2rem;
     text-decoration: none;
     text-transform: uppercase;
+    transition: all 80ms ease-in;
+  }
+
+  a:hover,
+  a:focus {
+    font-size: 1rem;
+    padding: 0.9rem 2.1rem;
   }
 
   @media ${mq.tablet} {
     a {
       font-size: 1.1rem;
       padding: 1.2rem 3.25rem;
+    }
+
+    a:hover,
+    a:focus {
+      font-size: 1.15rem;
+      padding: 1.3rem 3.4rem;
     }
   }
 `;
